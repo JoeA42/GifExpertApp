7f2c324bb5aaa9f2e144f05b176c921975716bdf
Joe_A <joe.alvarez42@gmail.com>
2022-08-01 14:45:45 -0600

    [Used new func component] Added new func component to GifExpertApp.jsx component

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react'
+import {AddCategory} from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch', "Dragon Ball"]);
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Valorant'])
+    }
+
     return (
         <>
 
             <h1>Gif Expert App</h1>
 
+            {/* Input */}
+
+            <AddCategory setCategories={ setCategories }/>
+
+            {/* Listado de gifs */}
+
+            <ol>
+                {categories.map( category => {
+                    return <li key={category}>{category}</li>
+                })}
+
+                {/*<li>123</li>*/}
+
+            </ol>
+            {/* Gif Item*/}
         </>
     )
 }
