dc24a13eea92335e501b7eac2d468f573d651615
Joe_A <joe.alvarez42@gmail.com>
2022-08-08 14:36:59 -0600

    [created index.js] created index to group exports into single file, as per best practices

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import {AddCategory} from "./components/AddCategory";
-import {GifGrid} from "./components/GifGrid.jsx";
+import {AddCategory, GifGrid} from "./components";
 
 export const GifExpertApp = () => {
 
