638bd68d6a0a05677839bfed65370ff9cdb22ede
Joe_A <joe.alvarez42@gmail.com>
2022-08-08 13:04:36 -0600

    [inline variable] Inlined variable and removed console log

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -3,21 +3,32 @@ import {useEffect, useState} from "react";
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
 
-    useEffect(()=> {
-        getGifs(category);
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
 
-    }, [])
+
+    useEffect(() => {
+            // noinspection JSIgnoredPromiseFromCall
+            getImages();
+        },
+        [])
 
 
     return (
         <>
             <h3>{ category }</h3>
 
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter(counter+1) }>+1</button>
-
+            <ol>
+                {
+                    images.map( ({id, title}) => (
+                        <li key={id}>{title}</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
